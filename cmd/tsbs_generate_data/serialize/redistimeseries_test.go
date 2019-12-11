aa50f56e918aa7a605f3ddc81e762e02447b8023
filipecosta90 <filipecosta.90@gmail.com>
2019-12-11 23:56:48 +0000

    [fix] fixed circleci tests

--- cmd/tsbs_generate_data/serialize/redistimeseries_test.go
+++ cmd/tsbs_generate_data/serialize/redistimeseries_test.go
@@ -8,24 +8,17 @@ func TestRedisTimeSeriesSerializer(t *testing.T) {
 		{
 			desc:       "a regular Point",
 			inputPoint: testPointDefault,
-			output:     "TS.ADD cpu_usage_guest_nice{hostname=host_0|region=eu-west-1|datacenter=eu-west-1b} 1451606400 38.24311829 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest_nice\n",
+			output:     "TS.ADD cpu_usage_guest_nice{3840552926} 1451606400000 38.24311829 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest_nice\n",
 		},
 		{
 			desc:       "a regular Point using int as value",
 			inputPoint: testPointInt,
-			output:     "TS.ADD cpu_usage_guest{hostname=host_0|region=eu-west-1|datacenter=eu-west-1b} 1451606400 38 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest\n",
-		},
-		{
-			desc:       "a regular Point with multiple fields",
-			inputPoint: testPointMultiField,
-			output:     "TS.ADD cpu_big_usage_guest{hostname=host_0|region=eu-west-1|datacenter=eu-west-1b} 1451606400 5000000000 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname big_usage_guest\n" +
-						"TS.ADD cpu_usage_guest{hostname=host_0|region=eu-west-1|datacenter=eu-west-1b} 1451606400 38 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest\n" +
-						"TS.ADD cpu_usage_guest_nice{hostname=host_0|region=eu-west-1|datacenter=eu-west-1b} 1451606400 38.24311829 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest_nice\n",
+			output:     "TS.ADD cpu_usage_guest{3840552926} 1451606400000 38 LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest\n",
 		},
 		{
 			desc:       "a Point with no tags",
 			inputPoint: testPointNoTags,
-			output:     "TS.ADD cpu_usage_guest_nice 1451606400 38.24311829 LABELS measurement cpu fieldname usage_guest_nice\n",
+			output:     "TS.ADD cpu_usage_guest_nice{3558706393} 1451606400000 38.24311829 LABELS measurement cpu fieldname usage_guest_nice\n",
 		},
 	}
 
