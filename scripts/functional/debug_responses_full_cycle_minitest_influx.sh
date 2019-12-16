3fa639f8f9ff5d9901d1bf90bb45a23bf1cf3f78
filipecosta90 <filipecosta.90@gmail.com>
2019-12-16 13:51:20 +0000

    [add] working on improving functional and minitest scripts

--- scripts/functional/debug_responses_full_cycle_minitest_influx.sh
+++ scripts/functional/debug_responses_full_cycle_minitest_influx.sh
@@ -16,13 +16,31 @@ rm docs/responses/${FORMAT}_*
 DATABASE_PORT=${DATABASE_PORT:-8086}
 DATABASE_HOST=${DATABASE_HOST:-localhost}
 
-source ./../load_common.sh
+# All available query types (sorted alphabetically)
+QUERY_TYPES_ALL="\
+  cpu-max-all-1 \
+  cpu-max-all-8 \
+  double-groupby-1 \
+  double-groupby-5 \
+  double-groupby-all \
+  groupby-orderby-limit \
+  high-cpu-1 \
+  high-cpu-all \
+  lastpoint \
+  single-groupby-1-1-1 \
+  single-groupby-1-1-12 \
+  single-groupby-1-8-1 \
+  single-groupby-5-1-1 \
+  single-groupby-5-1-12 \
+  single-groupby-5-8-1"
 
+# What query types to generate
+QUERY_TYPES=${QUERY_TYPES:-$QUERY_TYPES_ALL}
 
 # generate data
 $GOPATH/bin/tsbs_generate_data --format ${FORMAT} --use-case cpu-only --scale=${SCALE} --seed=${SEED} --file /tmp/bulk_data/${FORMAT}_data
 
-for queryName in "single-groupby-1-1-1" "single-groupby-1-1-12" "single-groupby-1-8-1" "single-groupby-5-1-1" "single-groupby-5-1-12" "single-groupby-5-8-1" "cpu-max-all-1" "cpu-max-all-8" "double-groupby-1" "double-groupby-5" "double-groupby-all" "high-cpu-all" "high-cpu-1" "lastpoint" "groupby-orderby-limit"; do
+for queryName in $QUERY_TYPES; do
   echo "generating query: $queryName"
   $GOPATH/bin/tsbs_generate_queries --format ${FORMAT} --use-case cpu-only --scale=${SCALE} --seed=${SEED} \
     --queries=1 \
@@ -31,13 +49,12 @@ for queryName in "single-groupby-1-1-1" "single-groupby-1-1-12" "single-groupby-
 done
 
 until curl http://${DATABASE_HOST}:${DATABASE_PORT}/ping 2>/dev/null; do
-    echo "Waiting for InfluxDB"
-    sleep 1
+  echo "Waiting for InfluxDB"
+  sleep 1
 done
 
 # Remove previous database
 curl -X POST http://${DATABASE_HOST}:${DATABASE_PORT}/query?q=drop%20database%20benchmark
-
 
 # insert benchmark
 $GOPATH/bin/tsbs_load_${FORMAT} \
@@ -48,9 +65,9 @@ $GOPATH/bin/tsbs_load_${FORMAT} \
   --file=/tmp/bulk_data/${FORMAT}_data
 
 # queries benchmark
-for queryName in "single-groupby-1-1-1" "single-groupby-1-1-12" "single-groupby-1-8-1" "single-groupby-5-1-1" "single-groupby-5-1-12" "single-groupby-5-8-1" "cpu-max-all-1" "cpu-max-all-8" "double-groupby-1" "double-groupby-5" "double-groupby-all" "high-cpu-all" "high-cpu-1" "lastpoint" "groupby-orderby-limit"; do
+for queryName in $QUERY_TYPES; do
   echo "running query: $queryName"
   $GOPATH/bin/tsbs_run_queries_${FORMAT} --print-responses \
     --workers=1 \
-    --file /tmp/bulk_data/${FORMAT}_query_$queryName > docs/responses/${FORMAT}_$queryName.json
+    --file /tmp/bulk_data/${FORMAT}_query_$queryName >docs/responses/${FORMAT}_$queryName.json
 done
--- scripts/functional/debug_responses_full_cycle_minitest_redistimeseries.sh
+++ scripts/functional/debug_responses_full_cycle_minitest_redistimeseries.sh
@@ -9,6 +9,27 @@ SEED=${SEED:-"123"}
 PASSWORD=${PASSWORD:-"password"}
 FORMAT="redistimeseries"
 
+# All available query types (sorted alphabetically)
+QUERY_TYPES_ALL="\
+  cpu-max-all-1 \
+  cpu-max-all-8 \
+  double-groupby-1 \
+  double-groupby-5 \
+  double-groupby-all \
+  groupby-orderby-limit \
+  high-cpu-1 \
+  high-cpu-all \
+  lastpoint \
+  single-groupby-1-1-1 \
+  single-groupby-1-1-12 \
+  single-groupby-1-8-1 \
+  single-groupby-5-1-1 \
+  single-groupby-5-1-12 \
+  single-groupby-5-8-1"
+
+# What query types to generate
+QUERY_TYPES=${QUERY_TYPES:-$QUERY_TYPES_ALL}
+
 mkdir -p /tmp/bulk_data
 rm /tmp/bulk_data/${FORMAT}_*
 rm docs/responses/${FORMAT}_*
@@ -18,7 +39,7 @@ redis-cli flushall
 # generate data
 $GOPATH/bin/tsbs_generate_data --format ${FORMAT} --use-case cpu-only --scale=${SCALE} --seed=${SEED} --file /tmp/bulk_data/${FORMAT}_data
 
-for queryName in "single-groupby-1-1-1" "single-groupby-1-1-12" "single-groupby-1-8-1" "single-groupby-5-1-1" "single-groupby-5-1-12" "single-groupby-5-8-1" "cpu-max-all-1" "cpu-max-all-8" "double-groupby-1" "double-groupby-5" "double-groupby-all" "high-cpu-all" "high-cpu-1" "lastpoint" "groupby-orderby-limit"; do
+for queryName in $QUERY_TYPES; do
   $GOPATH/bin/tsbs_generate_queries --format ${FORMAT} --use-case cpu-only --scale=${SCALE} --seed=${SEED} \
     --queries=1 \
     --query-type $queryName \
@@ -31,10 +52,10 @@ $GOPATH/bin/tsbs_load_${FORMAT} \
   --file=/tmp/bulk_data/${FORMAT}_data
 
 # queries benchmark
-for queryName in "single-groupby-1-1-1" "single-groupby-1-1-12" "single-groupby-1-8-1" "single-groupby-5-1-1" "single-groupby-5-1-12" "single-groupby-5-8-1" "cpu-max-all-1" "cpu-max-all-8" "double-groupby-1" "double-groupby-5" "double-groupby-all" "high-cpu-all" "high-cpu-1" "lastpoint" "groupby-orderby-limit"; do
+for queryName in $QUERY_TYPES; do
   echo "running query: $queryName"
   $GOPATH/bin/tsbs_run_queries_${FORMAT} --print-responses \
     --workers=1 \
     --debug=3 \
-    --file /tmp/bulk_data/${FORMAT}_query_$queryName > docs/responses/${FORMAT}_$queryName.json
+    --file /tmp/bulk_data/${FORMAT}_query_$queryName >docs/responses/${FORMAT}_$queryName.json
 done
