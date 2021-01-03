3006d29f40280dde10becbd8b7e590aa9ab98a17
filipecosta90 <filipecosta.90@gmail.com>
2021-01-03 22:39:12 +0000

    [add] Added LoaderTestResult to persist ingestion benchmark results in a common format across targets

--- scripts/full_cycle_minitest/full_cycle_minitest_timescaledb.sh
+++ scripts/full_cycle_minitest/full_cycle_minitest_timescaledb.sh
@@ -6,7 +6,7 @@
 
 MAX_RPS=${MAX_RPS:-"0"}
 MAX_QUERIES=${MAX_QUERIES:-"1000"}
-PASSWORD=${PASSWORD:-""}
+PASSWORD=${PASSWORD:-"password"}
 
 mkdir -p /tmp/bulk_data
 
@@ -19,7 +19,7 @@ $GOPATH/bin/tsbs_generate_queries --queries=${MAX_QUERIES} --format timescaledb 
 $GOPATH/bin/tsbs_generate_queries --queries=${MAX_QUERIES} --format timescaledb --use-case cpu-only --scale 10 --seed 123 --query-type high-cpu-1    --file /tmp/bulk_data/timescaledb_query_high-cpu-1
 
 # insert benchmark
-$GOPATH/bin/tsbs_load_timescaledb --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --db-name=benchmark --host=127.0.0.1 --user=postgres --workers=1 --file=/tmp/bulk_data/timescaledb_data
+$GOPATH/bin/tsbs_load_timescaledb --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --results-file="timescaledb_load_results.json" --db-name=benchmark --host=127.0.0.1 --user=postgres --workers=1 --file=/tmp/bulk_data/timescaledb_data
 
 # queries benchmark
 $GOPATH/bin/tsbs_run_queries_timescaledb --max-rps=${MAX_RPS} --hdr-latencies="${MAX_RPS}rps_timescaledb_query_lastpoint.hdr" --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --db-name=benchmark --hosts=127.0.0.1 --user=postgres --workers=1 --max-queries=${MAX_QUERIES} --file=/tmp/bulk_data/timescaledb_query_lastpoint
