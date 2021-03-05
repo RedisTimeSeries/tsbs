04661285acf6ba97abf98ef9db68e46621078911
filipecosta90 <filipecosta.90@gmail.com>
2021-03-05 12:32:02 +0000

    [add] Improved load script

--- scripts/load_redistimeseries.sh
+++ scripts/load_redistimeseries.sh
@@ -11,23 +11,16 @@ source ${EXE_DIR}/redistimeseries_common.sh
 # Ensure RESULTS DIR available
 mkdir -p ${RESULTS_DIR}
 
-for run in $(seq ${REPETITIONS}); do
-  echo "Running RUN $run"
-  OUT_FULL_FILE_NAME="${RESULTS_DIR}/${PREFIX}_load_result_run_${run}.out"
-  echo "Using only 1 worker"
-  echo "Saving results to ${OUT_FULL_FILE_NAME}"
+OUT_FULL_FILE_NAME="${RESULTS_DIR}/${PREFIX}_load_result.out"
+echo "Using only 1 worker"
+echo "Saving results to ${OUT_FULL_FILE_NAME}"
 
-    # Load new data
-  $EXE_FILE_NAME \
-    --file ${DATA_FILE_NAME} \
-    --workers=1 \
-    --batch-size=${BATCH_SIZE} \
-    --reporting-period=${REPORTING_PERIOD} \
-    --host=${DATABASE_HOST}:${DATABASE_PORT} \
-    --connections=${CONNECTIONS} --pipeline=${PIPELINE} |
-      tee ${OUT_FULL_FILE_NAME}
-
-  echo "Sleeping for ${SLEEP_BETWEEN_RUNS} seconds"
-  sleep ${SLEEP_BETWEEN_RUNS}
-
-done
+# Load new data
+$EXE_FILE_NAME \
+  --file ${DATA_FILE_NAME} \
+  --workers=1 \
+  --batch-size=${BATCH_SIZE} \
+  --reporting-period=${REPORTING_PERIOD} \
+  --host=${DATABASE_HOST}:${DATABASE_PORT} \
+  --connections=${CONNECTIONS} --pipeline=${PIPELINE} |
+  tee ${OUT_FULL_FILE_NAME}
