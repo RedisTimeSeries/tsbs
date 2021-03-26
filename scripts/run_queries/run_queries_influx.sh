4e576f12be8ad1e4e6378e37d6ab67788e4f9296
filipecosta90 <filipecosta.90@gmail.com>
2021-03-26 15:58:30 +0000

    [fix] Fixed influx runner

--- scripts/run_queries/run_queries_influx.sh
+++ scripts/run_queries/run_queries_influx.sh
@@ -10,27 +10,8 @@ fi
 DATABASE_PORT=${DATABASE_PORT:-8086}
 
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
-source ${EXE_DIR}/../load/load_common.sh
+source ${EXE_DIR}/run_common.sh
 
-# Results folder
-RESULTS_DIR=${RESULTS_DIR:-"./results"}
-
-# Debug
-DEBUG=${DEBUG:-0}
-
-# Default queries folder
-BULK_DATA_DIR=${BULK_DATA_DIR:-"/tmp/bulk_queries"}
-
-# How many queries would be run
-MAX_QUERIES=${MAX_QUERIES:-"0"}
-
-# How many concurrent worker would run queries - match num of cores, or default to 4
-NUM_WORKERS=${NUM_WORKERS:-$(grep -c ^processor /proc/cpuinfo 2>/dev/null || echo 4)}
-
-# Print timing stats to stderr after this many queries (0 to disable)
-QUERIES_PRINT_INTERVAL=${QUERIES_PRINT_INTERVAL:-"0"}
-
-REPETITIONS=${REPETITIONS:-3}
 
 until curl http://${DATABASE_HOST}:${DATABASE_PORT}/ping 2>/dev/null; do
     echo "Waiting for InfluxDB"
